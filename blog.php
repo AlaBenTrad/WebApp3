b104e631b32b703cc9213ae35d98f25bde83292a
Mohamed ala eddin ben trad <bentradala96@gmail.com>
2016-12-24 18:05:59 +0100

    Done.

---
+++ blog.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Véhicule| E-souk</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	
+   </head><!--/head-->
+
+<body>
+	<header id="header"><!--header-->
+		<div class="header_top"><!--header_top-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 ">
+						<div class="contactinfo">
+							<ul class="nav nav-pills">
+								<li><a href=""><i class="fa fa-phone"></i> +216 55 473 444</a></li>
+								<li><a href=""><i class="fa fa-envelope"></i> Esouk2016@gmail.com</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="https://www.facebook.com/E-souk-1450636968297513/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://twitter.com/E__souk" target="_blank"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://www.linkedin.com/home?trk=nav_responsive_tab_home" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="https://plus.google.com/u/2/" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header_top-->
+		
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<a href="index.php"><img src="images/home/e-souk.PNG" alt="" /></a>
+						</div>
+						<div class="btn-group pull-right"><!--MEP-->
+						</div>
+					</div>
+					<div class="col-sm-8">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								
+								<li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Panier</a></li>
+								<li><a href="login.html"><i class="fa fa-lock"></i> Se connecter</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-middle-->
+	
+		<div class="header-bottom"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="navbar-header">
+							
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="index.php">Acceuil</a></li>
+                                <li><a href="shop1.php">Boutique</a></li>
+                                 <li><a href="index.html">Blog</a></li>
+								<li><a href="contact-us.html">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+					
+				</div>
+				</div>
+			</div>
+	</header>
+	
+	<section id="advertisement">
+		<div class="container">
+			<img src="images/shop/ad.jpg" alt="" />
+		</div>
+	</section>
+	
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="left-sidebar">
+						<h2>CATÉGORIE</h2>
+						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="shop1.php">Multimédia</a></h4>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="shop2.php">Accessoires</a></h4>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="shop3.php">Meubles</a></h4>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="shop4.php">Véhicule</a></h4>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="shop5.php">Habillement</a></h4>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="shop6.php">Autre</a></h4>
+								</div>
+							</div>
+						</div><!--/category-productsr-->
+						
+						<div class="shipping text-center"><!--shipping-->
+							<img src="images/home/RoadShow2011.jpg" alt="" />
+						</div><!--/shipping-->
+						
+					</div>
+				</div>
+				
+				<div class="col-sm-9">
+					<div class="blog-post-area">
+						<h2 class="title text-center">Latest From our Blog</h2>
+						<div class="single-blog-post">
+							<h3>Girls Pink T Shirt arrived in store</h3>
+							<div class="post-meta">
+								<ul>
+									<li><i class="fa fa-user"></i> Mac Doe</li>
+									<li><i class="fa fa-clock-o"></i> 1:33 pm</li>
+									<li><i class="fa fa-calendar"></i> DEC 5, 2013</li>
+								</ul>
+								<span>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star-half-o"></i>
+								</span>
+							</div>
+							<a href="">
+								<img src="images/blog/blog-one.jpg" alt="">
+							</a>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+							<a  class="btn btn-primary" href="">Read More</a>
+						</div>
+						<div class="single-blog-post">
+							<h3>Girls Pink T Shirt arrived in store</h3>
+							<div class="post-meta">
+								<ul>
+									<li><i class="fa fa-user"></i> Mac Doe</li>
+									<li><i class="fa fa-clock-o"></i> 1:33 pm</li>
+									<li><i class="fa fa-calendar"></i> DEC 5, 2013</li>
+								</ul>
+								<span>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star-half-o"></i>
+								</span>
+							</div>
+							<a href="">
+								<img src="images/blog/blog-two.jpg" alt="">
+							</a>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+							<a  class="btn btn-primary" href="">Read More</a>
+						</div>
+						<div class="single-blog-post">
+							<h3>Girls Pink T Shirt arrived in store</h3>
+							<div class="post-meta">
+								<ul>
+									<li><i class="fa fa-user"></i> Mac Doe</li>
+									<li><i class="fa fa-clock-o"></i> 1:33 pm</li>
+									<li><i class="fa fa-calendar"></i> DEC 5, 2013</li>
+								</ul>
+								<span>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star-half-o"></i>
+								</span>
+							</div>
+							<a href="">
+								<img src="images/blog/blog-three.jpg" alt="">
+							</a>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+							<a  class="btn btn-primary" href="">Read More</a>
+						</div>
+						<div class="pagination-area">
+							<ul class="pagination">
+								<li><a href="" class="active">1</a></li>
+								<li><a href="">2</a></li>
+								<li><a href="">3</a></li>
+								<li><a href=""><i class="fa fa-angle-double-right"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	
+	<footer id="footer"><!--Footer-->
+		<div class="footer-top">
+			
+		
+		<div class="footer-widget">
+			<div class="container">
+				<div class="row">
+                	<div class="col-sm-2">
+						<div class="single-widget">
+							<ul class="nav nav-pills nav-stacked">
+                            <h2>Boutique</h2>
+                               <li><a href="shop1.php">Multimédia</a></li>
+								<li><a href="shop2.php">Accesoires</a></li>
+								<li><a href="shop3.php">Meubles</a></li>
+								<li><a href="shop4.php">Véhicule</a></li>
+								<li><a href="shop5.php">Habillement</a></li>
+                                <li><a href="shop6.php">Autre</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+                        <h2>Service</h2>
+							<ul class="nav nav-pills nav-stacked">
+                            <li><a href="index.php">Acceuil</a></li>	
+								<li><a href="">Contact </a></li>	
+                                <li><a href="">Blog </a></li>
+                                <li><a href="">Se connecter </a></li>							
+							</ul>						
+                            </div>
+					</div>
+				      <div class="col-sm-2">
+						<div class="single-widget">
+                        <h2>sites utiles</h2>
+							<ul class="nav nav-pills nav-stacked">
+                            <li><a href="https://www.tayara.tn" target="_blank">www.Tayara.tn</a></li>	
+								<li><a href="http://www.ebay.com/" target="_blank">www.ebay.com </a></li>	
+                                <li><a href="https://www.amazon.com" target="_blank">www.Amazone.com </a></li>
+                              	<li><a href="http://www.ldlc.com/" target="_blank">www.ldlc.com </a></li>					
+                                                   
+							</ul>						
+                            </div>
+					</div>
+				
+					<div class="col-sm-2">
+                    	<div class="single-widget">
+                        <h2>Visitez-nous</h2>
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="https://www.facebook.com/E-souk-1450636968297513/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://twitter.com/E__souk" target="_blank"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://www.linkedin.com/home?trk=nav_responsive_tab_home" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="https://plus.google.com/u/2/" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+							</ul>
+						</div>
+					</div>
+
+					</div>
+					<div class="col-sm-3 col-sm-offset-1">
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2013 E-Souk. Tous les droits sont réservés.</p>
+					
+				</div>
+			</div>
+		</div>
+		<script src="javaScript/Panier.js" type="text/javascript"></script>
+	</footer><!--/Footer-->
+</body>
+</html>
---
+++ panier.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Se connecter | E-souk</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<script src="javaScript/Panier.js" type="text/javascript"></script>
+   </head><!--/head-->
+
+<body>
+	<header id="header"><!--header-->
+		<div class="header_top"><!--header_top-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 ">
+						<div class="contactinfo">
+							<ul class="nav nav-pills">
+								<li><a href=""><i class="fa fa-phone"></i> +216 55 473 444</a></li>
+								<li><a href=""><i class="fa fa-envelope"></i> Esouk2016@gmail.com</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="https://www.facebook.com/E-souk-1450636968297513/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://twitter.com/E__souk" target="_blank"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://www.linkedin.com/home?trk=nav_responsive_tab_home" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="https://plus.google.com/u/2/" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header_top-->
+		
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<a href="index.php"><img src="images/home/e-souk.PNG" alt="" /></a>
+						</div>
+						<div class="btn-group pull-right"><!--MEP-->
+						</div>
+					</div>
+					<div class="col-sm-8">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								
+								<li><a href="panier.php"><i class="fa fa-shopping-cart"></i> Panier</a></li>
+								<li><a href="connect.php"><i class="fa fa-lock"></i> Se connecter</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-middle-->
+	
+		<div class="header-bottom"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="navbar-header">
+							
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="index.php">Acceuil</a></li>
+                                <li><a href="shop1.php">Boutique</a></li>
+                                 <li><a href="blog.php">Blog</a></li>
+								<li><a href="contact.php">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+					
+				</div>
+		  </div>
+	  </div>
+	</header>
+	
+<section id="advertisement">
+		<div class="container">
+			<img src="images/shop/ad.jpg" alt="" />
+		</div>
+	</section>
+	<div class="table-responsive cart_info">
+				<table class="table table-condensed">
+					<thead>
+						<tr class="cart_menu">
+							<td class="image">Item</td>
+							<td class="description"></td>
+							<td class="price">Price</td>
+							<td class="quantity">Quantity</td>
+							<td class="total">Total</td>
+							<td></td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="cart_product">
+								<a href=""><img src="images/cart/one.png" alt=""></a>
+							</td>
+							<td class="cart_description">
+								<h4><a href="">Colorblock Scuba</a></h4>
+								<p>Web ID: 1089772</p>
+							</td>
+							<td class="cart_price">
+								<p>$59</p>
+							</td>
+							<td class="cart_quantity">
+								<div class="cart_quantity_button">
+									<a class="cart_quantity_up" href=""> + </a>
+									<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
+									<a class="cart_quantity_down" href=""> - </a>
+								</div>
+							</td>
+							<td class="cart_total">
+								<p class="cart_total_price">$59</p>
+							</td>
+							<td class="cart_delete">
+								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+							</td>
+						</tr>
+
+						<tr>
+							<td class="cart_product">
+								<a href=""><img src="images/cart/two.png" alt=""></a>
+							</td>
+							<td class="cart_description">
+								<h4><a href="">Colorblock Scuba</a></h4>
+								<p>Web ID: 1089772</p>
+							</td>
+							<td class="cart_price">
+								<p>$59</p>
+							</td>
+							<td class="cart_quantity">
+								<div class="cart_quantity_button">
+									<a class="cart_quantity_up" href=""> + </a>
+									<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
+									<a class="cart_quantity_down" href=""> - </a>
+								</div>
+							</td>
+							<td class="cart_total">
+								<p class="cart_total_price">$59</p>
+							</td>
+							<td class="cart_delete">
+								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+							</td>
+						</tr>
+						<tr>
+							<td class="cart_product">
+								<a href=""><img src="images/cart/three.png" alt=""></a>
+							</td>
+							<td class="cart_description">
+								<h4><a href="">Colorblock Scuba</a></h4>
+								<p>Web ID: 1089772</p>
+							</td>
+							<td class="cart_price">
+								<p>$59</p>
+							</td>
+							<td class="cart_quantity">
+								<div class="cart_quantity_button">
+									<a class="cart_quantity_up" href=""> + </a>
+									<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
+									<a class="cart_quantity_down" href=""> - </a>
+								</div>
+							</td>
+							<td class="cart_total">
+								<p class="cart_total_price">$59</p>
+							</td>
+							<td class="cart_delete">
+								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</section> <!--/#cart_items-->
+	
+	<footer id="footer"><!--Footer-->
+		<div class="footer-top">
+			
+		
+		<div class="footer-widget">
+			<div class="container">
+				<div class="row">
+                	<div class="col-sm-2">
+						<div class="single-widget">
+							<ul class="nav nav-pills nav-stacked">
+                            <h2>Boutique</h2>
+                               <li><a href="shop1.php">Multimédia</a></li>
+								<li><a href="shop2.php">Accesoires</a></li>
+								<li><a href="shop3.php">Meubles</a></li>
+								<li><a href="shop4.php">Véhicule</a></li>
+								<li><a href="shop5.php">Habillement</a></li>
+                                <li><a href="shop6.php">Autre</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+                        <h2>Service</h2>
+							<ul class="nav nav-pills nav-stacked">
+                            <li><a href="index.php">Acceuil</a></li>	
+								<li><a href="contact.php">Contact </a></li>	
+                                <li><a href="blog.php">Blog </a></li>
+                                <li><a href="connect.php">Se connecter </a></li>												
+							</ul>						
+                      </div>
+					</div>
+				      <div class="col-sm-2">
+						<div class="single-widget">
+                        <h2>sites utiles</h2>
+							<ul class="nav nav-pills nav-stacked">
+                            <li><a href="https://www.tayara.tn" target="_blank">www.Tayara.tn</a></li>	
+								<li><a href="http://www.ebay.com/" target="_blank">www.ebay.com </a></li>	
+                                <li><a href="https://www.amazon.com" target="_blank">www.Amazone.com </a></li>
+                              	<li><a href="http://www.ldlc.com/" target="_blank">www.ldlc.com </a></li>					
+                                                   
+							</ul>						
+                        </div>
+					</div>
+				
+					<div class="col-sm-2">
+                    	<div class="single-widget">
+                        <h2>Visitez-nous</h2>
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="https://www.facebook.com/E-souk-1450636968297513/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://twitter.com/E__souk" target="_blank"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://www.linkedin.com/home?trk=nav_responsive_tab_home" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="https://plus.google.com/u/2/" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+							</ul>
+						</div>
+					</div>
+
+					</div>
+					<div class="col-sm-3 col-sm-offset-1">
+					</div>
+					
+				</div>
+			</div>
+</div>
+		
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2013 E-Souk. Tous les droits sont réservés.</p>
+					
+				</div>
+			</div>
+		</div>
+		
+	</footer><!--/Footer-->
+</body>
+</html>
